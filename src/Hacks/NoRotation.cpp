26b774149a63ff72501999c80c96fbfd3fdb1965
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-25 19:20:02 +0300

    Update NoRotation.cpp

--- src/Hacks/NoRotation.cpp
+++ src/Hacks/NoRotation.cpp
@@ -1,29 +1,29 @@
-#ifndef GEODE_IS_ANDROID32
+// #ifndef GEODE_IS_ANDROID32
 
-#include <Geode/Geode.hpp>
-#include <Geode/modify/CCNode.hpp>
-#include <Geode/modify/PlayerObject.hpp>
-#include "../Client/Client.h"
+// #include <Geode/Geode.hpp>
+// #include <Geode/modify/CCNode.hpp>
+// #include <Geode/modify/PlayerObject.hpp>
+// #include "../Client/Client.h"
 
-using namespace geode::prelude;
+// using namespace geode::prelude;
 
-Module* rot = nullptr;
+// Module* rot = nullptr;
 
-class $modify (PlayerObject)
-{
-    void setRotation(float r)
-    {
-        if (!rot)
-            rot = Client::GetModule("no-rot");
+// class $modify (PlayerObject)
+// {
+//    void setRotation(float r)
+//    {
+//        if (!rot)
+//            rot = Client::GetModule("no-rot");
+//
+//        if (rot->enabled)
+//           PlayerObject::setRotation(0);
+//        else
+//            PlayerObject::setRotation(r);
+//    }
+//};
 
-        if (rot->enabled)
-            PlayerObject::setRotation(0);
-        else
-            PlayerObject::setRotation(r);
-    }
-};
-
-#endif
+//#endif
 
 /*
 class $modify (CCNode)
